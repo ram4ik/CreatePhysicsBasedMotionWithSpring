35012e610295689121027348cc034d4fee331990
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-22 17:45:57 +0300

    init

--- CreatePhysicsBasedMotionWithSpring/ContentView.swift
+++ CreatePhysicsBasedMotionWithSpring/ContentView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rotateBell = false
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Text("Physics-based Animations/Motion")
+                .offset(y: -150)
+            Image(systemName: "bell.fill")
+                .font(.largeTitle)
+                .scaleEffect(2)
+                .foregroundColor(.blue)
+                .rotationEffect(.degrees(rotateBell ? -25 : 60), anchor: .top)
+                .animation(Animation.spring(
+                    response: 0.87,
+                    dampingFraction: 0.1,
+                    blendDuration: 5
+                    ).repeatForever(autoreverses: false).speed(4))
+                .onAppear() {
+                    self.rotateBell.toggle()
+            }
+            
+            Circle()
+                .frame(width: 25, height: 25)
+                .foregroundColor(.pink)
+                .offset(y: -35)
+            
+            Text("2")
+                .font(.body)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .offset(y: -35)
+        }
     }
 }
 
